f4cdf45b04d5c64d7c2cd91d3f7a8d7928014b1e
Cal Paterson <cal@calpaterson.com>
2019-12-02 21:10:38 +0000

    Log listening

--- src/extension/quartermarker.js
+++ src/extension/quartermarker.js
@@ -1,1 +1,22 @@
-console.log("loaded");
+function changeListener(id, changeInfo) {
+    console.log("changed: id: %s - %o", id, changeInfo);
+};
+
+function createdListener(id, bookmark) {
+    console.log("created: id: %s - %o", id, bookmark);
+};
+
+function movedListener(id, moveInfo) {
+    console.log("moved: id: %s - %o", id, moveInfo);
+};
+
+function removedListener(id, removeInfo) {
+    console.log("removed id: %s - %o", id, removeInfo);
+};
+
+browser.bookmarks.onChanged.addListener(changeListener);
+browser.bookmarks.onCreated.addListener(createdListener);
+browser.bookmarks.onMoved.addListener(movedListener);
+browser.bookmarks.onRemoved.addListener(removedListener);
+
+console.log("quartermarker loaded");
