2b88d02e165010001dcff478d0e49c41e48262df
Cal Paterson <cal@calpaterson.com>
2019-12-03 12:22:32 +0000

    More building stuff

--- src/server/setup.py
+++ src/server/setup.py
@@ -5,6 +5,9 @@ setup(
     version="0.0.1",
     packages=find_packages(),
     install_requires=["flask~=1.1.1", "flask-cors~=3.0.8"],
-    extras_require={"tests": ["pytest~=5.3.1",], "dev": ["black~=19.10b0",],},
+    extras_require={
+        "tests": ["pytest~=5.3.1",],
+        "dev": ["black~=19.10b0", "mypy~=0.750"],
+    },
     entry_points={"console_scripts": ["quartermarker=quartermarker:main",]},
 )
