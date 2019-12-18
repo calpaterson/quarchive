b5d6d3e68fa4b91ed9ef3abf7f361f33d55e873c
Cal Paterson <cal@calpaterson.com>
2019-12-18 15:12:05 +0000

    Yet more work towards full sync

--- src/extension/quartermarker.js
+++ src/extension/quartermarker.js
@@ -17,7 +17,7 @@ class Bookmark {
         // this.tags = tags;
     }
 
-    merge(other){
+    merge(other) {
         if (this.updated != other.updated) {
             if (this.updated > other.updated) {
                 return this
@@ -32,6 +32,40 @@ class Bookmark {
             }
         }
     }
+
+    to_json() {
+        return {
+            "url": this.url,
+            "timestamp": this.timestamp,
+            "title": this.title,
+            "unread": this.unread,
+            "deleted": this.deleted,
+        }
+    }
+
+    static from_json(json) {
+        return new this(
+            json.url,
+            json.title,
+            json.timestamp,
+            json.deleted,
+            json.unread,
+            json.browserId
+        )
+    }
+
+    static fromTreeNode(treeNode) {
+        // NOTE: as the bookmarks API does not provide unread or deleted
+        // information, those are both assumed to be false
+        return new this(
+            treeNode.url,
+            treeNode.title,
+            treeNode.dateAdded,
+            false,
+            false,
+            treeNode.id,
+        )
+    }
 }
 
 async function getCredentials() {
@@ -42,19 +76,33 @@ async function getCredentials() {
 
 // Lookup the bookmark from browser.bookmarks
 async function lookupBookmarkFromBrowser(browserId) {
-    // FIXME: this can fail, should check to make sure more than one treeNode
+    // FIXME: this can fail, should check to make sure no more than one
+    // treeNode
     const treeNodes = await browser.bookmarks.get(browserId)
     const treeNode = treeNodes[0];
-    const bookmark = new Bookmark(
-        treeNode.url,
-        treeNode.title,
-        treeNode.dateAdded,
-        false,
-        false,
-        browserId,
-    );
+    const bookmark = Bookmark.fromTreeNode(treeNode);
     console.log("built %o", bookmark);
     return bookmark;
+}
+
+async function allBookmarksFromBrowser() {
+    // cautiously create a new array rather than reusing because who knows what
+    // will happen if we mutate the array returned by getTree
+    var unexplored = [(await browser.bookmarks.getTree())[0]];
+    var bookmarks = [];
+    while (unexplored.length > 0) {
+        const treeNode = unexplored.pop();
+        if (treeNode.type === 'bookmark') {
+            bookmarks.push(Bookmark.fromTreeNode(treeNode));
+        }
+        if (Object.prototype.hasOwnProperty.call(treeNode, 'children')
+            && treeNode.children.length > 0) {
+            for (var child of treeNode.children) {
+                unexplored.push(child);
+            }
+        }
+    }
+    return bookmarks;
 }
 
 async function upsertBookmarkIntoBrowser(bookmark) {
@@ -73,30 +121,6 @@ async function upsertBookmarkIntoBrowser(bookmark) {
     }
 }
 
-// Lookup the bookmark from local db
-async function lookupBookmarkFromLocalDbByBrowserId(browserId) { // FIXME: ByBrowserId
-    return new Promise(function(resolve, reject) {
-        var transaction = db.transaction(["bookmarks"], "readonly");
-        transaction.oncomplete = function(event){
-            console.log("lookupBookmarkFromLocalDbByBrowserId transaction complete: %o", event);
-        }
-        transaction.onerror = function(event){
-            console.warn("lookupBookmarkFromLocalDbByBrowserId transaction failed: %o", event);
-        }
-        var objectStore = transaction.objectStore("bookmarks");
-        var index = objectStore.index("browserId");
-        var request = index.get(browserId);
-        request.onsuccess = function(event){
-            console.log("lookupBookmarkFromLocalDbByBrowserId request complete: %o", event);
-            resolve(request.result);
-        }
-        request.onerror = function(event){
-            console.warn("lookupBookmarkFromLocalDbByBrowserId request failed: %o", event);
-            reject(); // FIXME: what if it doesn't exist?
-        }
-    });
-}
-
 async function allBookmarksFromLocalDb() {
     return new Promise(function(resolve, reject) {
         var transaction = db.transaction(["bookmarks"], "readonly");
@@ -107,8 +131,7 @@ async function allBookmarksFromLocalDb() {
             console.warn("allBookmarksFromLocalDb transaction failed: %o", event);
         }
         var objectStore = transaction.objectStore("bookmarks");
-        var index = objectStore.index("browserId");
-        var request = index.getAll()
+        var request = objectStore.getAll()
         request.onsuccess = function(event){
             console.log("allBookmarksFromLocalDb request complete: %o", event);
             resolve(request.result);
@@ -133,11 +156,43 @@ async function lookupBookmarkFromLocalDbByUrl(url) {
         var request = objectStore.get(url)
         request.onsuccess = function(event){
             console.log("lookupBookmarkFromLocalDbByUrl request complete: %o", event);
-            resolve(request.result);
+            if (request.result === undefined){
+                resolve(null);
+            } else {
+                resolve(Bookmark.from_json(request.result));
+            }
         }
         request.onerror = function(event){
             console.warn("lookupBookmarkFromLocalDbByUrl request failed: %o", event);
-            reject(); // FIXME: what if it doesn't exist?
+            reject()
+        }
+    });
+}
+
+// Lookup the bookmark from local db
+async function lookupBookmarkFromLocalDbByBrowserId(browserId) {
+    return new Promise(function(resolve, reject) {
+        var transaction = db.transaction(["bookmarks"], "readonly");
+        transaction.oncomplete = function(event){
+            console.log("lookupBookmarkFromLocalDbByBrowserId transaction complete: %o", event);
+        }
+        transaction.onerror = function(event){
+            console.warn("lookupBookmarkFromLocalDbByBrowserId transaction failed: %o", event);
+        }
+        var objectStore = transaction.objectStore("bookmarks");
+        var index = objectStore.index("browserId");
+        var request = index.get(browserId);
+        request.onsuccess = function(event){
+            console.log("lookupBookmarkFromLocalDbByBrowserId request complete: %o", event);
+            if (request.result === undefined) {
+                resolve(null);
+            } else {
+                resolve(Bookmark.from_json(request.result));
+            }
+        }
+        request.onerror = function(event){
+            console.warn("lookupBookmarkFromLocalDbByBrowserId request failed: %o", event);
+            reject();
         }
     });
 }
@@ -187,20 +242,28 @@ async function updateBookmarkInLocalDb(bookmark){
     });
 }
 
-async function copyBrowserBookmarksToLocalDb(){
-    // TODO
+async function syncBrowserBookmarksToLocalDb() {
+    console.log("starting syncBrowserBookmarksToLocalDb");
+    const browserBookmarks = await allBookmarksFromBrowser();
+    for (var browserBookmark of browserBookmarks) {
+        const localBookmark = await lookupBookmarkFromLocalDbByUrl(browserBookmark.url);
+        if (localBookmark === null) {
+            await insertBookmarkIntoLocalDb(browserBookmark);
+        } else {
+            const merged = localBookmark.merge(browserBookmark);
+            if (merged !== localBookmark) {
+                console.log("%s out of date in local db, updating", merged.url);
+                await updateBookmarkInLocalDb(merged);
+            }
+        }
+    }
+    console.log("completed syncBrowserBookmarksToLocalDb");
 }
 
 // Syncs a bookmark with the API
 async function syncBookmark(bookmark) {
     const sync_body = {
-        "bookmarks": [{  // FIXME: refactor this into to_json and from_json
-            "url": bookmark.url,
-            "timestamp": bookmark.timestamp,
-            "title": bookmark.title,
-            "unread": bookmark.unread,
-            "deleted": bookmark.deleted,
-        }]};
+        "bookmarks": [bookmark.to_json()]};
     console.log("syncing %o", sync_body);
     const [username, APIKey] = await getCredentials();
     // FIXME: failure should be logged
@@ -224,7 +287,7 @@ async function fullSyncBookmarks(bookmarks){
 
 async function fullSync() {
     console.log("starting full sync");
-    await copyBrowserBookmarksToLocalDb();
+    await syncBrowserBookmarksToLocalDb();
     const bookmarksFromLocalDb = await allBookmarksFromLocalDb();
     const bookmarksFromServer = await fullSyncBookmarks(bookmarksFromLocalDb);
     // delete bookmarksFromLocalDb // should consider this, it's probably pretty large
@@ -285,10 +348,12 @@ dbOpenRequest.onupgradeneeded = function (event) {
 dbOpenRequest.onsuccess = function(event){
     console.log("opened database: %o, %o", event, dbOpenRequest.result);
     db = dbOpenRequest.result;
-    browser.bookmarks.onChanged.addListener(changeListener);
-    browser.bookmarks.onCreated.addListener(createdListener);
-    browser.bookmarks.onMoved.addListener(movedListener);
-    browser.bookmarks.onRemoved.addListener(removedListener);
+    syncBrowserBookmarksToLocalDb().then(function() {
+        browser.bookmarks.onChanged.addListener(changeListener);
+        browser.bookmarks.onCreated.addListener(createdListener);
+        browser.bookmarks.onMoved.addListener(movedListener);
+        browser.bookmarks.onRemoved.addListener(removedListener);
+    });
 };
 
 
