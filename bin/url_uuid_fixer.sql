9f43ff976291dbe342aefa8d9d7b6481e5bc9219
Cal Paterson <cal@calpaterson.com>
2020-05-17 17:05:09 +0100

    Url uuids fixed in prod - commiting minor fixes done along the way

--- bin/url_uuid_fixer.sql
+++ bin/url_uuid_fixer.sql
@@ -4,7 +4,7 @@ ALTER TABLE bookmarks DROP CONSTRAINT bookmarks_url_fkey;
 
 ALTER TABLE crawl_requests DROP CONSTRAINT crawl_requests_url_uuid_fkey;
 
--- ALTER TABLE full_text DROP CONSTRAINT full_text_url_uuid_fkey;
+ALTER TABLE full_text DROP CONSTRAINT full_text_url_uuid_fkey;
 
 UPDATE urls
 SET url_uuid = to_uuid
@@ -25,11 +25,11 @@ FROM url_fixes
 WHERE url_uuid = from_uuid
 ;
 
--- UPDATE full_text
--- SET uuid_uuid = to_uuid
--- WHERE url_fixes
--- WHERE url_uuid = from_uuid
--- ;
+UPDATE full_text
+SET url_uuid = to_uuid
+FROM url_fixes
+WHERE url_uuid = from_uuid
+;
 
 ALTER TABLE bookmarks
 ADD CONSTRAINT bookmarks_url_fkey
@@ -39,8 +39,8 @@ ALTER TABLE crawl_requests
 ADD CONSTRAINT crawl_requests_url_uuid_fkey
 FOREIGN KEY (url_uuid) REFERENCES urls (url_uuid);
 
--- ALTER TABLE full_text
--- ADD CONSTRAINT full_text_url_uuid_fkey
--- FOREIGN KEY (url_uuid) REFERENCES urls (url_uuid);
+ALTER TABLE full_text
+ADD CONSTRAINT full_text_url_uuid_fkey
+FOREIGN KEY (url_uuid) REFERENCES urls (url_uuid);
 
-ROLLBACK;
+-- ROLLBACK;
