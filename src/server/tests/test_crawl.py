645d4b1fa7abf17e6a1e8b09dbe131949bd0e97b
Cal Paterson <cal@calpaterson.com>
2020-09-30 19:27:06 +0100

    Move over the majority of tests to the new crawler file

--- src/server/tests/test_crawl.py
+++ src/server/tests/test_crawl.py
@@ -12,7 +12,8 @@ from freezegun import freeze_time
 import pytest
 
 import quarchive as sut
-from quarchive.data.functions import upsert_url
+from quarchive.data.functions import upsert_url, set_bookmark
+from quarchive.data.models import CrawlRequest, CrawlResponse, SQLAUrl
 from quarchive.value_objects import URL
 from quarchive import crawler
 from .conftest import random_string, make_bookmark
@@ -22,7 +23,7 @@ pytestmark = pytest.mark.crawler
 
 @pytest.fixture(scope="session", autouse=True)
 def lower_requests_timeout():
-    with mock.patch.object(sut, "REQUESTS_TIMEOUT", 0.1):
+    with mock.patch.object(crawler, "REQUESTS_TIMEOUT", 0.1):
         yield
 
 
@@ -40,8 +41,8 @@ def test_crawl_when_response_is_recieved(session, status_code, mock_s3):
     crawl_uuid = uuid4()
     crawler.crawl_url(session, crawl_uuid, url)
 
-    request = session.query(sut.CrawlRequest).get(crawl_uuid)
-    response = session.query(sut.CrawlResponse).get(crawl_uuid)
+    request = session.query(CrawlRequest).get(crawl_uuid)
+    response = session.query(CrawlResponse).get(crawl_uuid)
 
     assert request.requested == datetime(2018, 1, 3, tzinfo=timezone.utc)
     assert request.got_response
@@ -50,7 +51,7 @@ def test_crawl_when_response_is_recieved(session, status_code, mock_s3):
     assert response.headers == {"content-type": "text/plain"}
 
     s3_obj = (
-        sut.get_s3()
+        crawler.get_s3()
         .Object(environ["QM_RESPONSE_BODY_BUCKET_NAME"], str(response.body_uuid))
         .get()
     )
@@ -60,43 +61,48 @@ def test_crawl_when_response_is_recieved(session, status_code, mock_s3):
 
 @responses.activate
 def test_crawl_when_no_response(session):
-    url = "http://example.com/"
+    url = URL.from_string("http://example.com/" + random_string())
+    upsert_url(session, url)
+
     responses.add(
-        responses.GET, url, body=requests.exceptions.ConnectTimeout("connect timeout")
+        responses.GET,
+        url.to_string(),
+        body=requests.exceptions.ConnectTimeout("connect timeout"),
     )
 
     crawl_uuid = uuid4()
-    sut.crawl_url(session, crawl_uuid, url)
+    crawler.crawl_url(session, crawl_uuid, url)
 
-    request = session.query(sut.CrawlRequest).get(crawl_uuid)
-    response = session.query(sut.CrawlResponse).get(crawl_uuid)
+    request = session.query(CrawlRequest).get(crawl_uuid)
+    response = session.query(CrawlResponse).get(crawl_uuid)
     assert request is not None
     assert response is None
 
 
 @responses.activate
 def test_ensure_crawled_only_runs_once(session, mock_s3):
-    url = "http://example.com/" + random_string()
+    url = URL.from_string("http://example.com/" + random_string())
+    upsert_url(session, url)
 
-    responses.add(responses.GET, url, body=b"hello", stream=True)
+    responses.add(responses.GET, url.to_string(), body=b"hello", stream=True)
 
-    sut.ensure_crawled(url)
+    crawler.ensure_url_is_crawled(session, url)
 
-    s, n, p, q, f = urlsplit(url)
+    s, n, p, q, f = urlsplit(url.to_string())
     resp_query = (
-        session.query(sut.CrawlResponse)
-        .join(sut.CrawlRequest)
-        .join(sut.SQLAUrl)
+        session.query(CrawlResponse)
+        .join(CrawlRequest)
+        .join(SQLAUrl)
         .filter(
-            sut.SQLAUrl.scheme == s,
-            sut.SQLAUrl.netloc == n,
-            sut.SQLAUrl.path == p,
-            sut.SQLAUrl.query == q,
-            sut.SQLAUrl.fragment == f,
+            SQLAUrl.scheme == s,
+            SQLAUrl.netloc == n,
+            SQLAUrl.path == p,
+            SQLAUrl.query == q,
+            SQLAUrl.fragment == f,
         )
     )
     assert resp_query.count() == 1
-    sut.ensure_crawled(url)
+    crawler.ensure_url_is_crawled(session, url)
 
     # Assert again
     assert resp_query.count() == 1
@@ -105,7 +111,7 @@ def test_ensure_crawled_only_runs_once(session, mock_s3):
 @responses.activate
 def test_enqueue_crawls_for_uncrawled_urls(session, eager_celery, mock_s3, test_user):
     bookmark = make_bookmark()
-    sut.set_bookmark(session, test_user.user_uuid, bookmark)
+    set_bookmark(session, test_user.user_uuid, bookmark)
     session.commit()
     url = bookmark.url
 
@@ -113,15 +119,15 @@ def test_enqueue_crawls_for_uncrawled_urls(session, eager_celery, mock_s3, test_
     sut.enqueue_crawls_for_uncrawled_urls()
 
     resp_query = (
-        session.query(sut.CrawlResponse)
-        .join(sut.CrawlRequest)
-        .join(sut.SQLAUrl)
+        session.query(CrawlResponse)
+        .join(CrawlRequest)
+        .join(SQLAUrl)
         .filter(
-            sut.SQLAUrl.scheme == url.scheme,
-            sut.SQLAUrl.netloc == url.netloc,
-            sut.SQLAUrl.path == url.path,
-            sut.SQLAUrl.query == url.query,
-            sut.SQLAUrl.fragment == url.fragment,
+            SQLAUrl.scheme == url.scheme,
+            SQLAUrl.netloc == url.netloc,
+            SQLAUrl.path == url.path,
+            SQLAUrl.query == url.query,
+            SQLAUrl.fragment == url.fragment,
         )
     )
     assert resp_query.count() == 1
