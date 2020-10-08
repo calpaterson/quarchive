cfa261322bbfb3c317136cc46511ed8ba7b40e5e
Cal Paterson <cal@calpaterson.com>
2020-10-08 15:49:15 +0100

    Upgrade to Kombu 5

--- src/server/setup.py
+++ src/server/setup.py
@@ -30,7 +30,7 @@ setup(
         "python-magic~=0.4.15",
         "pyhash~=0.9.3",
         "pyappcache~=0.1",
-        "kombu~=4.6.8",
+        "kombu~=5.0.0",
         "attrs~=20.2.0",
     ],
     extras_require={
