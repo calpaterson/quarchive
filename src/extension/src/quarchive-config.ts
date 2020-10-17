4674e9b394e37e6535d3eb25bd57f1a2de6436e3
Cal Paterson <cal@calpaterson.com>
2020-10-17 21:13:20 +0100

    Remove dead config code
    This will need to be rethought later

--- src/extension/src/quarchive-config.ts
+++ src/extension/src/quarchive-config.ts
@@ -1,28 +1,5 @@
 import {randomUUID} from "./quarchive-uuid.js"
 
-// export class Config {
-//     apiKey: string;
-//     username: string;
-//     apiURL: string;
-
-//     constructor(apiKey: string, username: string, apiURL: string){
-//         this.apiKey = apiKey;
-//         this.username = username;
-//         this.apiURL = apiURL;
-//     }
-// }
-
-// export async function getConfig(): Promise<Config> {
-//     let fromStorage = await browser.storage.sync.get(["APIKey", "username", "APIURL"]);
-
-//     if (fromStorage.APIURL
-
-//     let apiKey = await browser.storage.sync.get("APIKey") as Optional<string>;
-//     let username = await browser.storage.sync.get("username") as Optional<string>;
-//     let apiURL = await browser.storage.sync.get("APIURL") as Optional<string>;
-
-//     return new Config(apiKey, username, apiURL)
-// }
 
 export async function getClientID(): Promise<string> {
     let wrappedClientID = await browser.storage.sync.get("clientID");
