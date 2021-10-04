6c176f4db4c1e1e9a8c2d9263fd7a3b80315b736
Cal Paterson <cal@calpaterson.com>
2021-10-04 15:40:07 +0100

    Update test to account for slightly different mimetypes for favicons
    Looks like magic on new systems returns something slightly different

--- src/server/tests/test_favicon.py
+++ src/server/tests/test_favicon.py
@@ -3,9 +3,11 @@ import magic
 
 
 def test_favicon(client):
+    ALLOWED_MIMES = ["image/vnd.microsoft.icon", "image/x-icon"]
+
     response = client.get("/favicon.ico")
     with tempfile.TemporaryFile(mode="w+b") as temp_file:
         temp_file.write(response.data)
         temp_file.seek(0)
         mime = magic.from_buffer(temp_file.read(2048), mime=True)
-    assert mime == "image/x-icon"
+    assert mime in ALLOWED_MIMES
